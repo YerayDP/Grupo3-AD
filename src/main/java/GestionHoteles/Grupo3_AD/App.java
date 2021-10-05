9e7182719153e83486f1e203cab26ef4b6a04dbf
YerayDP <yerayd99@gmail.com>
2021-10-05 09:52:21 +0200

    G3

---
+++ src/main/java/GestionHoteles/Grupo3_AD/App.java
@@ -0,0 +1,13 @@
+package GestionHoteles.Grupo3_AD;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
