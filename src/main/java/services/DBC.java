5d07e866fb3177b877651f6b011c4d213eb019de
YerayDP <73529878+YerayDP@users.noreply.github.com>
2021-10-05 19:35:53 +0200

    DBC

--- src/main/java/services/DBC.java
+++ src/main/java/services/DBC.java
@@ -7,7 +7,8 @@ import java.sql.SQLException;
 public class DBC {
 	
 	private static String dbhost = "jdbc:mysql://localhost:3306/gestionhoteles";
-
+	private static String username = "root";
+	private static String password = "";
 	private static Connection conn;
 	
 
@@ -23,7 +24,7 @@ public class DBC {
 	public static Connection createNewDBconnection() {
 		try  {	
 			conn = DriverManager.getConnection(
-					dbhost, "root", "");
+					dbhost, username, password);
 			System.out.println("Connected");
 		} catch (SQLException e) {
 			System.out.println("Cannot create database connection");
