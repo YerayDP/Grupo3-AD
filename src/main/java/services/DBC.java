5216ebca74ed9504885e0fba2337f36526502a36
YerayDP <73529878+YerayDP@users.noreply.github.com>
2021-10-05 19:23:42 +0200

    Connection

---
+++ src/main/java/services/DBC.java
@@ -0,0 +1,36 @@
+package services;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBC {
+	
+	private static String dbhost = "jdbc:mysql://localhost:3306/gestionhoteles";
+	private static String username = "root";
+	private static String password = "";
+	private static Connection conn;
+	
+
+	
+	public static void main(String[] args) {
+		
+		createNewDBconnection();		
+	}
+	
+	
+	
+	@SuppressWarnings("finally")
+	public static Connection createNewDBconnection() {
+		try  {	
+			conn = DriverManager.getConnection(
+					dbhost, username, password);
+			System.out.println("Connected");
+		} catch (SQLException e) {
+			System.out.println("Cannot create database connection");
+			e.printStackTrace();
+		} finally {
+			return conn;	
+		}		
+	}
+}
