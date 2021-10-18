a4c5efe1590ad81bba9b7a17875ec2f8603b4606
pablodelgadom <pablo00dm00@gmail.com>
2021-10-18 23:13:16 +0200

    fff

--- src/main/java/views/VerDatosC.java
+++ src/main/java/views/VerDatosC.java
@@ -19,6 +19,8 @@ import models.Empleados;
 import services.DBC;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -129,7 +131,109 @@ public class VerDatosC extends JFrame {
 				}
 			}
 		});
+		
+		final DefaultTableModel modelo = new DefaultTableModel();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        final Connection con = DBC.createNewDBconnection();
+
+        String sql = "Select id, dni, nombre, apellidos, fecha_nacimiento, imagen, username,password from users WHERE dni=?";
+
+        ps = con.prepareStatement(sql);
+
+        ps.setString(1,c.getDni());
+
+        rs = ps.executeQuery();
+
+        ResultSetMetaData rsMd = rs.getMetaData();
+
+        int cantidadColumnas = rsMd.getColumnCount();
+
+        modelo.addColumn("ID");
+        modelo.addColumn("DNI");
+        modelo.addColumn("Nombre");
+        modelo.addColumn("Apellidos");
+        modelo.addColumn("Fecha_nacimiento");
+        modelo.addColumn("Imagen");
+        modelo.addColumn("Username");
+        modelo.addColumn("Password");
+
+        while (rs.next()) {
+            Object[] filas = new Object[cantidadColumnas];
+
+            for (int i = 0; i < cantidadColumnas; i++) {
+                filas[i] = rs.getObject(i + 1);
+            }
+            modelo.addRow(filas);
+        }
+		
+		
+		 final JTable table_1 = new JTable(modelo);
+	        table_1.setEnabled(false);
+	        table_1.setBounds(10, 11, 596, 16);
+	        contentPane.add(table_1);
+	        table_1.setVisible(false);
+	        
 		btnNewButton_5.setBounds(10, 10, 72, 33);
 		contentPane.add(btnNewButton_5);
+		
+		JButton btnNewButton = new JButton("Modificar datos");
+        btnNewButton.setBounds(52, 312, 189, 55);
+        contentPane.add(btnNewButton);
+        btnNewButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                
+                String dni = modelo.getValueAt(0, 1).toString();
+                String nombre = modelo.getValueAt(0, 2).toString();
+                String apellidos = modelo.getValueAt(0, 3).toString();
+                String fecha_nacimiento = modelo.getValueAt(0,4).toString();
+                String imagen = modelo.getValueAt(0, 5).toString();
+                String username = modelo.getValueAt(0, 6).toString();
+                String password = modelo.getValueAt(0, 7).toString();
+        
+                String NewDNI= JOptionPane.showInputDialog(null,"Introduzca nuevo DNI",dni);
+                String NewNombre= JOptionPane.showInputDialog(null,"Introduzca nuevo Nombre",nombre);
+                String NewApellidos= JOptionPane.showInputDialog(null,"Introduzca nuevos Apellidos",apellidos);
+                String NewFecha_nacimiento= JOptionPane.showInputDialog(null,"Introduzca nueva Fecha de nacimiento",fecha_nacimiento);
+                String NewImagen= JOptionPane.showInputDialog(null,"Introduzca nueva imagen",imagen);
+                String NewUsername= JOptionPane.showInputDialog(null,"Introduzca nuevo username",username);
+                String NewPassword= JOptionPane.showInputDialog(null,"Introduzca nuevo password",password);
+                
+
+                String sql = "UPDATE users\r\n"
+                        + "SET dni = " + NewDNI + " , nombre = '" + NewNombre + "' , apellidos = '" + NewApellidos + 
+                        "' , fecha_nacimiento = '" + NewFecha_nacimiento + "' , imagen = '" + NewImagen + "' , username = '" + NewUsername + "' , password = '" + NewPassword +"' WHERE dni = '"+c.getDni()+"'" ;
+            
+                try {
+                    PreparedStatement pst = con.prepareStatement(sql);
+                    pst.execute();
+                    modelo.fireTableDataChanged();
+                    JTable table_2 = new JTable(modelo);
+                    table_2.setBounds(50, 48, 690, 220);
+                    
+                    
+                    contentPane.add(table_2);
+                    contentPane.invalidate();
+                    contentPane.validate();
+                    contentPane.repaint();
+                    JOptionPane.showMessageDialog(null, "Actualizado");
+                    setVisible(false);
+                    Cliente cs = new Cliente(c);
+
+                    cs.setVisible(true);
+                } catch (SQLException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+                
+                
+                
+
+            }
+        });
+		
 }
 }
